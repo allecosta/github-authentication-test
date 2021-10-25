940926d3773920ce7b9cdaf0d8b1cce9019b2be0
allecosta <alle_costa10@outlook.com>
2021-10-25 15:23:30 -0300

    finalized test

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
 	<H2>Teste de Autenticação de Repositório Remoto</H2>
 
 	<p>
-		Recentemente, mais precisamente a partir de 13 de agosto de 2021, o github fez um update na forma de autenticação para subir projetos do repositório local para o remoto. Desde então, coincidência ou não, tenho tido alguns problemas para atualizar os meus projetos. 	
+		Recentemente, mais precisamente a partir de 13 de agosto de 2021, o github fez um update na forma de autenticação para subir projetos do repositório local para o remoto. 
+		Desde então, coincidência ou não, tenho tido alguns problemas para atualizar os meus projetos :-(
 	</p>
 
 	<?php 
@@ -29,6 +30,11 @@
 	}
 
 	?>
+
+	<p>
+		Teste realizado com sucesso! Conseguir upar este teste lá para o meu repositório remoto utilizando uma chave SSH.
+		Vale lembrar que há uma segunda forma de autenticação que seria via Token.
+	</p>
 
 </body>
 
